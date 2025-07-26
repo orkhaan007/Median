27e2b6c5828b182fe69fa9d4d7cc04d7029b458f
Orkhaan007 <orkhanm07@gmail.com>
2025-07-26 13:25:45 -0700

    MedianV1

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { Geist } from "next/font/google";
+import "./globals.scss";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+const geistSans = Geist({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={geistSans.className} suppressHydrationWarning>
+      <body className="">
+        {children}
+      </body>
+    </html>
+  );
+}
